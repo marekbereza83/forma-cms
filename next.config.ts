cc493a8a0aa858f2c2c35d0e0b08aa0bbc5c9d4c
Marek Bereza <marekbereza34@gmail.com>
2026-05-26 14:22:44 +0200

    allow cloudflare r2 images

--- next.config.ts
+++ next.config.ts
@@ -10,21 +10,6 @@ const nextConfig: NextConfig = {
       },
     ]
   },
-}
-
-export default nextConfig
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: false,
-      },
-    ]
-  },
 
   images: {
     remotePatterns: [
